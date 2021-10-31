6ddb145eab96253793ce48bb347cff23c94749c7
LuiisGustavo <lg0401@gmail.com>
2021-10-31 19:33:22 -0300

    Initial commit

---
+++ src/main/java/br/com/chronosAcademy/appium/CalculadoraTest.java
@@ -0,0 +1,65 @@
+package br.com.chronosAcademy.appium;
+import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculadoraTest {
+
+    @Test
+    public void validarMultiplicacao() throws MalformedURLException {
+        DriverFactory driverFactory = new DriverFactory();
+        driverFactory.setCapabilities("Android", "ChronosMobile", "uiautomator2",
+                "com.android.calculator2", "com.android.calculator2.Calculator");
+        driverFactory.setDriver();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_7")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("op_mul")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_9")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("eq")).click();
+        String resultado = driverFactory.getDriver().findElement(MobileBy.id("result")).getText();
+
+        assertEquals("63", resultado);
+        driverFactory.getDriver().quit();
+
+    }
+
+    @Test
+    public void validarSoma() throws MalformedURLException {
+        DriverFactory driverFactory = new DriverFactory();
+        driverFactory.setCapabilities("Android", "ChronosMobile", "uiautomator2",
+                "com.android.calculator2", "com.android.calculator2.Calculator");
+        driverFactory.setDriver();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_7")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("op_add")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_9")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("eq")).click();
+        String resultado = driverFactory.getDriver().findElement(MobileBy.id("result")).getText();
+
+        assertEquals("16", resultado);
+        driverFactory.getDriver().quit();
+    }
+
+    @Test
+    public void validarDivisao() throws MalformedURLException {
+        DriverFactory driverFactory = new DriverFactory();
+        driverFactory.setCapabilities("Android", "ChronosMobile", "uiautomator2",
+                "com.android.calculator2", "com.android.calculator2.Calculator");
+        driverFactory.setDriver();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_8")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_0")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("op_div")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_2")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("eq")).click();
+        String resultado = driverFactory.getDriver().findElement(MobileBy.id("result")).getText();
+
+        assertEquals("40", resultado);
+        driverFactory.getDriver().quit();
+
+    }
+
+
+}
+
+
